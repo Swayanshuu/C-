29d987cbf355172aaae9d8cd382b45fa76736e19
Swayanshu sarthak sadangi <swayanshu19@gmail.com>
2024-10-24 15:31:48 +0530

    Here is about time complexity in C++.
    It is so understandable and also very necessary.

---
+++ time complexity/ex1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   int n;
+   cin>>n;
+   int a=0;
+   for(int i=1;i<=n;i++){
+    a=a+1;/*can change 1 to i for another operation*/
+    /*here n is directly propoltional to i i.e called linierly propotional*/
+   } 
+   cout<<a<<endl;
+    return 0;
+}
+
+/*time coplexity*/
---
+++ time complexity/ex2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+    int a=0;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            a=a+1;
+        }
+        cout<<a<<endl;
+
+    }
+    return 0;
+}/*running time is n squre*/
