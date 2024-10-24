355fff865712e3c027a59c9c2223e1e086214c58
Swayanshu sarthak sadangi <swayanshu19@gmail.com>
2024-10-24 15:28:47 +0530

    All about funtion in C++

---
+++ FUNCTION/q5.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;  /*Pascle Triangle*/
+
+int fact(int n){
+    int f=1;
+    for(int i=2;i<=n;i++){
+        f*=i;
+    }
+    return f;
+}
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<fact(i)/(fact(j)*fact(i-j))<<" ";
+
+        }
+        cout<<endl;
+
+   }
+   return 0; 
+}
